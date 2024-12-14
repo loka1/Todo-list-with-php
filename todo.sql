c9d029b67ff69e85a26e7a6c05ef41dc70d8dc70
lokas <lokashafeek7755@gmail.com>
2024-12-14 21:57:49 +0200

    update sql

--- todo.sql
+++ todo.sql
@@ -1,60 +1,54 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.39, for Linux (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 28, 2020 at 05:58 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: todo_app
+-- ------------------------------------------------------
+-- Server version	8.0.39-0ubuntu0.20.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `todo`
---
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `task_table`
 --
 
+DROP TABLE IF EXISTS `task_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `task_table` (
-  `id` int(5) NOT NULL,
-  `task_name` varchar(300) NOT NULL,
-  `added_tiime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `task_name` mediumtext COLLATE utf8mb4_unicode_ci,
+  `added_tiime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=42 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Indexes for dumped tables
+-- Dumping data for table `task_table`
 --
 
---
--- Indexes for table `task_table`
---
-ALTER TABLE `task_table`
-  ADD PRIMARY KEY (`id`);
+/*!40000 ALTER TABLE `task_table` DISABLE KEYS */;
+INSERT INTO `task_table` VALUES (32,'aaaaaaaaaaa','2024-12-14 18:18:18'),(35,'شششش','2024-12-14 18:36:22'),(36,'ششششششش','2024-12-14 18:36:25'),(37,'ششششششششaaaaaa','2024-12-14 18:36:26'),(38,'aaaaaaaaaaaaa','2024-12-14 18:39:42'),(39,'aaaaaaaaaa','2024-12-14 18:57:02'),(40,'aaaaaaaaa','2024-12-14 18:57:38'),(41,'aaaaaaaaa','2024-12-14 19:00:54');
+/*!40000 ALTER TABLE `task_table` ENABLE KEYS */;
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Dumping routines for database 'todo_app'
 --
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- AUTO_INCREMENT for table `task_table`
---
-ALTER TABLE `task_table`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-14 21:45:06
